fd1128378d86be845252b80830ba131361590534
Pontes <hpo.pontes@gmail.com>
2023-05-17 15:43:06 -0300

    compreendendo melhor funções

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 //console.log para informar os erros
-let robotron = document.getElementById('robotron')
+//o querySelector tenho que passar se é #id ou .class
+const robotron = document.getElementById('robotron')
 //adiciona o evento para o elemento
 robotron.addEventListener('click',()=>{
-    console.log('Olá')
+    console.log("Olá")
+    console.log('Bem vindo ao robotron 2000')
 });
+// função anonima , economizamos na escrita usando 'arrows function'
+//parentezes vem dentro dos parentezes
+//usasse crase para poder acessar o valor da vaiavel no js
+
+function dizOi(nome){
+    console.log(`Olá, ${nome}`)
+}
+dizOi("pontes")
