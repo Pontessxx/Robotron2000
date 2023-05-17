991ab81bd98934f0b007a0509a0b5f8ce777bb90
Pontes <hpo.pontes@gmail.com>
2023-05-17 17:09:18 -0300

    interação de estatisticas com soma e subtração

--- script.js
+++ script.js
@@ -20,10 +20,44 @@ dizOi("pontes")
      somar = document.querySelector('#somar'),
      braco = document.querySelector('#braco'), */
 const controle = document.querySelectorAll('[data-controle]')
-
+const estatistica = document.querySelectorAll('[data-estatistica]')
+const pecas = {
+    "braco": {
+        "forca": 29,
+        "poder": 35,
+        "energia": -21,
+        "velocidade": -5
+    },
+    "blindagem": {
+        "forca": 41,
+        "poder": 20,
+        "energia": 0,
+        "velocidade": -20
+    },
+    "nucleos": {
+        "forca": 0,
+        "poder": 7,
+        "energia": 48,
+        "velocidade": -4
+    },
+    "pernas": {
+        "forca": 27,
+        "poder": 21,
+        "energia": -32,
+        "velocidade": 43
+    },
+    "foguetes": {
+        "forca": 0,
+        "poder": 28,
+        "energia": 0,
+        "velocidade": -2
+    }
+}
+//console.log(estatistica)
 controle.forEach((elemento)=>{
     elemento.addEventListener('click',(evento)=>{
-        manipulaDados(evento.target.dataset.controle, evento.target.parentNode)
+        manipulaDados(evento.target.dataset.controle, evento.target.parentNode);
+        atualizaEstatistica(evento.target.dataset.peca)
     })
 })
 
@@ -35,6 +69,9 @@ function manipulaDados(operacao, controle){
         peca.value = parseInt(peca.value) + 1;
     }
 };
-/* Aprendemos como utilizar a propriedade parentNode;
-Definimos o que é data-attributes;
-Criamos funções para manipular elementos no DOM com data-attributes. */
+function atualizaEstatistica(peca){
+    //console.log(pecas[peca])
+    estatistica.forEach((elemento)=>{
+        elemento.textContent = parseInt(elemento.textContent) + pecas[peca][elemento.dataset.estatistica]
+    })
+}
