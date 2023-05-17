7732bca9d5ed746a38a72304fb8b41effb8abcb7
Pontes <hpo.pontes@gmail.com>
2023-05-17 16:15:49 -0300

    utilizando funcao para melhor legibilidade de codico

--- script.js
+++ script.js
@@ -20,9 +20,13 @@ const subtrair = document.getElementById('subtrair'),
      somar = document.getElementById('somar'),
      braco = document.getElementById('braco')
 
-somar.addEventListener('click', (evento)=>{
-    braco.value = parseInt(braco.value) + 1;
-})
-subtrair.addEventListener('click', (evento)=>{
-    braco.value = parseInt(braco.value) - 1;
-})
+somar.addEventListener('click', ()=>{manipulaDados('somar')});
+subtrair.addEventListener('click', ()=>{manipulaDados('subtrair')});
+
+function manipulaDados(operacao){
+    if(operacao === 'subtrair'){
+        braco.value = parseInt(braco.value) - 1;
+    }else{
+        braco.value = parseInt(braco.value) + 1;
+    }
+};
