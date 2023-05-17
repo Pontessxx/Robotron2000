f0e5c164d75bdee88ebda20af2190d45a310326d
Pontes <hpo.pontes@gmail.com>
2023-05-17 15:28:04 -0300

    usando o addEventListener

---
+++ script.js
@@ -0,0 +1,6 @@
+//console.log para informar os erros
+let robotron = document.getElementById('robotron')
+//adiciona o evento para o elemento
+robotron.addEventListener('click',()=>{
+    console.log('Olá')
+});
